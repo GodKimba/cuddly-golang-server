2df14eee8f36d0070bd6577ae5f6ebe39621e1de
GodKimba <rafaelfadel2@gmail.com>
2022-05-05 08:11:16 -0300

    started the server program, added the gitignore file and the html files

--- server.go
+++ server.go
@@ -6,8 +6,27 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	// This return a error if method is not GET
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
 func main() {
+	// Handling the response and request
+	http.HandleFunc("/hello", helloHandler)
+
 	fmt.Printf("Starting server at port 8080\n")
+	// Starting server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
