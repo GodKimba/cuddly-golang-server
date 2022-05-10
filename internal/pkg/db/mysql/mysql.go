48794239de2503099e2ed2eabffb81901ecbd0b3
GodKimba <rafaelfadel2@gmail.com>
2022-05-10 07:35:37 -0300

    created all the internal folders for organization, the sql db for the user and links using docker

---
+++ internal/pkg/db/mysql/mysql.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/mysql"
+	_ "github.com/golang-migrate/migrate/source/file"
+	"log"
+)
+
+var Db *sql.DB
+
+func InitDB() {
+	db, err := sql.Open("mysql", "root:dbpass@tcp(localhost)/hackernews")
+	if err != nil {
+		log.Panic(err)
+	}
+}
