827a794d54b47f4dc5a43faca06db18c50bf7119
GodKimba <rafaelfadel2@gmail.com>
2022-05-11 08:46:37 -0300

    created the init db and migrate db function and fixed an import error

--- internal/pkg/db/mysql/mysql.go
+++ internal/pkg/db/mysql/mysql.go
@@ -2,11 +2,16 @@ package database
 
 import (
 	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang-migrate/migrate"
 	"github.com/golang-migrate/migrate/database/mysql"
 	_ "github.com/golang-migrate/migrate/source/file"
-	"log"
 )
 
 var Db *sql.DB
@@ -16,4 +21,42 @@ func InitDB() {
 	if err != nil {
 		log.Panic(err)
 	}
+
+	if err = db.Ping(); err != nil {
+		log.Panic(err)
+	}
+	Db = db
+}
+
+func Migrate() {
+	if err := Db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	driver, _ := mysql.WithInstance(Db, &mysql.Config{})
+	m, _ := migrate.NewWithDatabaseInstance(
+		"file://internal/pkg/db/migrations/mysql",
+		"mysql",
+		driver,
+	)
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
+	github.com/go-chi/chi}
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	database.InitDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(hackernews.NewExecutableSchema(hackernews.Config{Resolvers: &hackernews.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
